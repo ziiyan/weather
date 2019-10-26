f8895396ae5f4440edd35172b7584ebd66e06910
wuziyan <wuziyan@hyperchain.cn>
2019-10-26 13:49:35 +0800

    add database & table settings

---
+++ app/src/main/java/com/example/weather/db/City.java
@@ -0,0 +1,37 @@
+package com.example.weather.db;
+
+import org.litepal.crud.LitePalSupport;
+
+public class City extends LitePalSupport {
+
+    private int id;
+
+    private String cityName;
+
+    private int cityCode;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCityName () {
+        return cityName;
+    }
+
+    public void setCityName(String cityName) {
+        this.cityName = cityName;
+    }
+
+    public int getCityCode() {
+        return cityCode;
+    }
+
+    public void setCityCode(int cityCode) {
+        this.cityCode = cityCode;
+    }
+
+}
